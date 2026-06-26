9f010346c6b761895d6263cd6465b289a787cea9
Joe Watkins <krakjoe@php.net>
2026-06-26 08:48:30 +0200

    license updates in ext

--- src/glr/chart.c
+++ src/glr/chart.c
@@ -4,13 +4,8 @@
   +----------------------------------------------------------------------+
   | Copyright (c) Joe Watkins 2026                                       |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
+  | This source file is subject to the BSD 3-Clause License bundled     |
+  | with this package in the file LICENSE.                               |
   +----------------------------------------------------------------------+
   | Author: krakjoe                                                      |
   +----------------------------------------------------------------------+
